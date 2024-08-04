6dc3584722ac476118335afa9499198d78b9d2c2
HeladoDeUvaNelson <nelson227@hotmail.es>
2024-08-03 22:10:32 -0300

    utils folder added and security keycerts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,12 +1,7 @@
 import Express from "express";
-import fs from "fs";
-const crypto = require('crypto');
-
-
-const SHA: string | undefined = process.env.SHA;
+import { encryptString, decryptString } from "../utils/security";
 
 const authMiddleware = (req: Express.Request, res: Express.Response, next: Express.NextFunction) => {
-    if (!SHA) return;
     next()
 }
 
---
+++ src/utils/security.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { publicEncrypt, privateDecrypt } from "crypto";
+import { parseKey, parsePrivateKey } from "sshpk";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SHA: string | undefined = process.env.SHA;
+
+export const encryptString = (plaintext: string, publicKeyFile: string) => { 
+    const publicKey = fs.readFileSync(publicKeyFile, "utf8"); 
+
+    const pemKey = parseKey(publicKey, 'ssh');
+    const cypherText = publicEncrypt(pemKey.toBuffer("pem"), Buffer.from(plaintext, 'utf8'));
+
+    return cypherText.toString("base64"); 
+} 
+
+export const decryptString = (privateKeyFile: string, cypherText: string) => { 
+    const privateKey = fs.readFileSync(privateKeyFile, "utf8"); 
+
+    const pemKey = parsePrivateKey(privateKey, 'ssh');
+    const decryptMess = privateDecrypt(pemKey.toBuffer("pem"), Buffer.from(cypherText, "base64"))
+
+    return decryptMess.toString("utf8"); 
+}
